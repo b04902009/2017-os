aa9cfa396b7b0be97815aaabb3577dc62304a295
os <os@os-VirtualBox.(none)>
2017-04-26 08:31:43 +0800

    hw2 part2 half

--- hw2/part1/sched_test.c
+++ hw2/part1/sched_test.c
@@ -1,11 +1,15 @@
 #define _GNU_SOURCE
 #include <pthread.h>
 #include <sched.h>
+#include <stdio.h>
+#include <unistd.h>
 
 void *thread_func(void * num){
     int k = num;
-    k++;
-    printf("thread %d\n",k);
+    for (int i =0;i<3;i++){
+        printf("Thread %d is running\n",k);
+        sleep(1); 
+    }
 }
 
 int main(int argc,char * argv[]){
@@ -15,9 +19,16 @@ int main(int argc,char * argv[]){
     pthread_attr_init(&attr);
     CPU_ZERO(&cpus);
     CPU_SET(0,&cpus);
-
+    
+    if (argc == 2){
+        printf("%s\n",argv[1]);
+    
+    }
     for (int i =0 ; i<2 ; i++){
         pthread_attr_setaffinity_np(&attr,sizeof(cpu_set_t),&cpus);
+        printf ("??? %d\n",i);
         pthread_create(&threads[i],&attr,thread_func,(void *) i);
     }
+    for (int i =0 ; i<2 ;i++)
+        pthread_join(threads[i],NULL);
 }
