f19d2c0df6e588a6d2ceb653764815bd73dbd983
os <os@os-VirtualBox.(none)>
2017-04-26 09:34:48 +0800

    done hw2 part1

--- hw2/part1/sched_test.c
+++ hw2/part1/sched_test.c
@@ -3,7 +3,7 @@
 #include <sched.h>
 #include <stdio.h>
 #include <unistd.h>
-
+#include <assert.h>
 void *thread_func(void * num){
     int k = num;
     k++;
@@ -19,10 +19,25 @@ int main(int argc,char * argv[]){
     pthread_attr_t attr;
     cpu_set_t cpus;
     pthread_attr_init(&attr);
-    
+    struct sched_param param[2];
+   
+
     if (argc == 2){
         printf("%s\n",argv[1]);
+        for (int i =0 ; i<2 ; i++){
+            param[i].sched_priority = sched_get_priority_max(SCHED_FIFO)-i;
     
+            CPU_ZERO(&cpus);
+            CPU_SET(0,&cpus);
+            pthread_attr_setaffinity_np(&attr,sizeof(cpu_set_t),&cpus);
+            
+            pthread_attr_setinheritsched(&attr, PTHREAD_EXPLICIT_SCHED);
+            pthread_attr_setschedpolicy(&attr,SCHED_FIFO);
+            pthread_attr_setschedparam(&attr,&param[i]);
+            
+            assert(pthread_create(&threads[i],&attr,thread_func,(void *) i)==0);
+        }
+        
     }
     else {
         for (int i =0 ; i<2 ; i++){
